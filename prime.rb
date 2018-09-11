a8575acbc734650bf93efa838bed2bedf9eb9857
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-11 01:43:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,16 @@
 # Add  code here!
 def prime?(num)
- prime = true
- range = 2...num
- range.each do |int|
-    if num % int == 0 || int <= 1
-      return prime = false
+  if num <= 1
+    return prime = false
+  else
+   prime = true
+   range = 2...num
+   range.each do |int|
+      if num % int == 0
+        return prime = false
+      end
     end
+    prime
   end
-  prime
 end
     
